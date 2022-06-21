e323eb899fb3380ae70c6a6a22f6460457aa3dfe
Divyarya-code <71401691+Divyarya-code@users.noreply.github.com>
2022-06-21 15:56:17 +0530

    initial commit

---
+++ views/JS/custData.js
@@ -0,0 +1,51 @@
+let custData = [{
+    "account_no": "0001",
+    "name": "Anurag",
+    "email": "anurag@gmail.com",
+    "balance": "478945.00"
+  }, {
+    "account_no": "0002",
+    "name": "Shailesh",
+    "email": "shailesh@pinterest.com",
+    "balance": "83154.98"
+  }, {
+    "account_no": "0003",
+    "name": "Devesh Singh",
+    "email": "devesh@histats.com",
+    "balance": "254384.79"
+  }, {
+    "account_no": "0004",
+    "name": "Aditya kumar",
+    "email": "aditya@hotmail.com",
+    "balance": "211287.86"
+  }, {
+    "account_no": "0005",
+    "name": "Nikhil Singh",
+    "email": "nikhil@yahoo.com",
+    "balance": "609438.80"
+  }, {
+    "account_no": "0006",
+    "name": "Akash tiwari",
+    "email": "akash@outlook.net",
+    "balance": "90902.84"
+  }, {
+    "account_no": "0007",
+    "name": "Vishal pandey",
+    "email": "vishal@gmail.com",
+    "balance": "9952.50"
+  }, {
+    "account_no": "0008",
+    "name": "Akash Bhagne",
+    "email": "akash@live.in",
+    "balance": "119087.60"
+  }, {
+    "account_no": "0009",
+    "name": "Prince Singh",
+    "email": "prince@gmail.com",
+    "balance": "238421.32"
+  }, {
+    "account_no": "0010",
+    "name": "Aasna tiwari",
+    "email": "aasna@live.com",
+    "balance": "5667680.26"
+  }];
